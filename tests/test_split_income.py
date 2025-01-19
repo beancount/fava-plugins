5a129171bfb76d0c5cb4cf55e3897aacaadfe081
Oliver Papst <oliver.papst@mailbox.org>
2025-01-19 21:39:50 +0100

    Fix(split_income): Convert positions to weight before subtracting taxes (#7)
    Previously, the code assumed that income and taxes used the same
    currency. If the postings had an additional associated cost or price,
    this was not considered. Now, to obtain the correct amount, the
    beancount.core.convert.get_weight function is used. It provides the
    amount that will need to be balanced from a posting of a transaction.
    
    Appropriate tests are included.

--- tests/test_split_income.py
+++ tests/test_split_income.py
@@ -99,3 +99,77 @@ def test_split_income_config(load_doc):
 
     assert len(entries) == 9
     assert len([e for e in entries if isinstance(e, data.Open)]) == 7
+
+
+def test_split_income_mixed_currency_income(load_doc):
+    """
+    plugin "fava_plugins.split_income" ""
+    plugin "beancount.plugins.auto_accounts"
+
+    2025-01-01 * "Employer" "Income"
+        Income:Work                       -10 USD @@ 9 EUR
+        Expenses:Taxes                      1 EUR
+        Assets:Account                      8 EUR
+    """
+
+    entries, errors, __ = load_doc
+
+    entries_after, _, __ = load_string(
+        """
+    2025-01-01 * "Employer" "Income"
+        Assets:Account                      8 EUR
+        Income:Net:Work                    -8 EUR
+
+    2025-01-01 * "Employer" "Income" #pretax
+        Income:Work                       -10 USD @ 0.9 EUR
+        Expenses:Taxes                     1 EUR
+        Income:Net:Work                    8 EUR
+    """,
+        dedent=True,
+    )
+
+    assert not errors
+    assert "pretax" in entries[5].tags
+
+    _compare_postings(entries[5], entries_after[1])
+    _compare_postings(entries[4], entries_after[0])
+
+    assert len(entries) == 6
+    assert len([e for e in entries if isinstance(e, data.Open)]) == 4
+
+
+def test_split_income_mixed_currency_others(load_doc):
+    """
+    plugin "fava_plugins.split_income" ""
+    plugin "beancount.plugins.auto_accounts"
+
+    2025-01-01 * "Employer" "Income"
+        Income:Work                       -11 USD
+        Expenses:Taxes                      1 EUR @ 1.1 USD
+        Assets:Account                      9 EUR @ 1.1 USD
+    """
+
+    entries, errors, __ = load_doc
+
+    entries_after, _, __ = load_string(
+        """
+    2025-01-01 * "Employer" "Income"
+        Assets:Account                      9 EUR @ 1.1 USD
+        Income:Net:Work                  -9.9 USD
+
+    2025-01-01 * "Employer" "Income" #pretax
+        Income:Work                       -11 USD
+        Expenses:Taxes                     1 EUR @ 1.1 USD
+        Income:Net:Work                    9.9 USD
+    """,
+        dedent=True,
+    )
+
+    assert not errors
+    assert "pretax" in entries[5].tags
+
+    _compare_postings(entries[5], entries_after[1])
+    _compare_postings(entries[4], entries_after[0])
+
+    assert len(entries) == 6
+    assert len([e for e in entries if isinstance(e, data.Open)]) == 4
